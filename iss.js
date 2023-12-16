fb208d2aa27f1f58c4548b27b842cd84ef2feb47
LiamGunning07 <liamgunning07@gmail.com>
2023-12-15 17:12:47 -0700

    "fetchCoordsByIP function complete"

--- iss.js
+++ iss.js
@@ -1,4 +1,5 @@
 const request = require('request');
+
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) return callback(error, null);
@@ -13,6 +14,24 @@ const fetchMyIP = function(callback) {
   });
 };
   
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+  
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
 
-module.exports = { fetchMyIP };
+    const data = JSON.parse(body);
+    const coords = {
+      latitude: data.latitude,
+      longitude: data.longitude
+    };
+  
+    callback(null, coords);
+  });
+
+};
+module.exports = { fetchMyIP, fetchCoordsByIP };
 
